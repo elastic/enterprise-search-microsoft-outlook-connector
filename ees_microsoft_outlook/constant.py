07a5d465f765d92914751bb406d538b98aaadcb4
moxarth-elastic <moxarth.rathod@elastic.co>
2022-06-02 18:01:33 +0530

    add checkpointing

--- ees_microsoft_outlook/constant.py
+++ ees_microsoft_outlook/constant.py
@@ -6,7 +6,10 @@
 """This module contains all the constants used throughout the code.
 """
 
+import datetime
+
 RFC_3339_DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 BATCH_SIZE = 100
 CONNECTOR_TYPE_OFFICE365 = "Office365"
 CONNECTOR_TYPE_MICROSOFT_EXCHANGE = "Microsoft Exchange"
+CURRENT_TIME = (datetime.datetime.utcnow()).strftime("%Y-%m-%dT%H:%M:%SZ")
