a28fc2e425631867a54d79eade07532d82e85f71
moxarth-elastic <moxarth.rathod@elastic.co>
2022-07-11 16:14:43 +0530

    make minor changes

--- ees_microsoft_outlook/cli.py
+++ ees_microsoft_outlook/cli.py
@@ -4,6 +4,7 @@
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
 """Cli module contains entry points to the connector.
+
 Each method provides a way to make an interaction
 between Elastic Enterprise Search and remote system.
 """
