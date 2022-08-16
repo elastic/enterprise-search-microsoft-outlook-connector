aa12de2359e970584366cc9917856d1cb32c5dff
moxarth-elastic <moxarth.rathod@elastic.co>
2022-08-16 11:25:56 +0530

    update schema

--- ees_microsoft_outlook/schema.py
+++ ees_microsoft_outlook/schema.py
@@ -89,6 +89,7 @@ schema = {
         "type": "string",
         "default": "Office365",
         "allowed": ["Office365", "Microsoft Exchange"],
+        "empty": False,
     },
     "enable_document_permission": {
         "required": False,
@@ -156,11 +157,17 @@ schema = {
         "allowed": ["DEBUG", "INFO", "WARNING", "ERROR"],
     },
     "retry_count": {"required": False, "type": "integer", "default": 3, "min": 1},
-    "source_sync_thread_count": {"required": True, "type": "integer", "default": 5},
+    "source_sync_thread_count": {
+        "required": True,
+        "type": "integer",
+        "default": 5,
+        "min": 1,
+    },
     "enterprise_search_sync_thread_count": {
         "required": True,
         "type": "integer",
         "default": 5,
+        "min": 1,
     },
     "connector.user_mapping": {"required": False, "type": "string"},
 }
